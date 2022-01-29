a8da619d029f154e0781bc189097c8419164a3e9
jerrett.hoffman <jerrett.hoffman@workday.com>
2022-01-28 23:25:20 -0800

    Finishes Trie Implementation

--- Trie.py
+++ Trie.py
@@ -1,49 +1,56 @@
 #Python Implementation of the Trie for WordleBot
 #Assumes that all words added to Trie are the same length (5 characters with all lower case letters)
 
-class TrieNode:
-	"""The Node of the Trie"""
-	def __init__(self):
-		#Initialize one child for each letter to null value
-		self.children = [None]*26
+def charToIndex(chr):
+        #returns a lowercase character encoded as an integer with 'a' == 0 and 'z' == 25
+        return ord(chr) - ord('a')
 
-		#Initialize value to mark end of word to false
-		self.isEndOfWord = False
+class TrieNode:
+        """The Node of the Trie"""
+        def __init__(self):
+                #Initialize one child for each letter to null value
+                self.children = [None]*26
+
+                #Initialize value to mark end of word to false
+                self.isEndOfWord = False
+
+        def whatAmI(self):
+                print("I am a TrieNode")
+                pass
 
 
 class Trie:
-	"""The Trie Iteslf"""
-	def __init__(self):
-		self.root = self.getNode()
+        """The Trie Iteslf"""
+        def __init__(self):
+                self.root = self.getNode()
 
-	#returns TrieNode initialized to null
-	def getNode(self):
-		return TrieNode()
+        #returns TrieNode initialized to null
+        def getNode(self):
+                return TrieNode()
 
+        def insert(self,key):
+                pCrawl = self.root
+                length = len(key)
+                for l in range(length):
+                        index = key[l]
+                        index = charToIndex(index)
+                        if not pCrawl.children[index]:
+                                pCrawl.children[index] = self.getNode()
+                        #Iterate the pCrawl
+                        pCrawl = pCrawl.children[index]
 
-	def insert(self,key):
-		currentLevel= self.root
-		length = len(key)
-		
-		for l in range(length):
-			index = key[l]
-			if not currentLevel.children[index]:
-				currentLevel.children[index] = self.getNode
-			#Iterate the currentlevel
-			currentLevel = currentLevel.children[index]
+                #mark the last node as the leaf
+                pCrawl.isEndOfWord = True
 
-		#mark the last node as the leaf
-		currentLevel.isEndOfWord = True
+        def search(self,key):
+                pCrawl = self.root
+                length = len(key)
 
+                for l in range(length):
+                        index = key[l]
+                        index = charToIndex(index)
+                        if not pCrawl.children[index]:
+                                return False
+                        pCrawl= pCrawl.children[index]
 
-	def search(self,key):
-		currentLevel= self.root
-		length = len(key)
-
-		for l in range(length):
-			index = key[l]
-			if not currentLevel.children[index]:
-				return False
-			currentLevel= currentLevel[index]
-
-		return currentLevel.isEndOfWord
+                return pCrawl.isEndOfWord
